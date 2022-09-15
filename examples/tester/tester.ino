c624e040c012657ba3bbf2773de9ad247a1f3bc3
An7or <antor.mee@gmail.com>
2022-09-15 18:03:24 +0600

    example

--- examples/tester/tester.ino
+++ examples/tester/tester.ino
@@ -1,10 +1,10 @@
-#include <DigitLedDisplay.h>
+#include <MAX7219Segment.h>
 
 #define DIN 12
 #define CLK 11
 #define CS  10
 
-DigitLedDisplay display = DigitLedDisplay(DIN, CS, CLK);
+MAX7219Segment display = MAX7219Segment(DIN, CS, CLK);
 
 void setup() {
   display.setBright(10); // 1 - 15
